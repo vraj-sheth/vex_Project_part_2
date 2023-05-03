d2253f2c789cc75920dfef93b9dccb7f5248dc1a
Vraj Sheth <vshe779@aucklanduni.ac.nz>
2023-05-03 16:20:59 +1200

    473

--- src/Student_Code.c
+++ src/Student_Code.c
@@ -31,6 +31,7 @@ double pivotWheelRatio = 114.0;    // number of encoder ticks per 1 revolution o
 /* Write your code in the function below. You may add helper functions below the studentCode function. */
 void student_Main(){
 {
+    armUp(4000);
 double dis2can = readSensor(SonarSensor);
 delay(50);
 drive_to_can(320);
@@ -50,11 +51,13 @@ drivePcont(10.0,0.8,0.1);
 delay(50);
 linefollowing();
 delay(50);
-drivePcont(254.0,0.8,0.1);
+drivePcont(254.0,0.8,0.1);// might need to edit
 delay(50);
-moveArmAngle(-13.5,2);
+moveArmAngle(-13.5,2.0);
 delay(50);
 drivePcont(-150.0,0.8,0.1);
+delay(50);
+armUp(4000);
 delay(50);
 rotateAngle(7.0,-90.0);
 delay(50);
